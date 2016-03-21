94828a6c84653ed9556c82a9502b20b2352b9938
Pillai <nmbpillai@gmail.com>
2016-03-21 01:59:12 -0400

    Update alexnet.lua

--- alexnet.lua
+++ alexnet.lua
@@ -1,4 +1,4 @@
-equire 'cudnn'
+require 'cudnn'
 
 
 function convLayer(a)
@@ -26,7 +26,43 @@ function buildAlexNet()
 
    local cnn = nn.Sequential()
    print(cnn)
+   return cnn
 end
 
 
-buildAlexNet()
+------------------load images & classes ----
+local train_dir = '/hdd2/datasets/imagenet/train256max/'
+local test_dir = '/hdd2/datasets/imagenet/val/'
+
+train_set = torch.load(train_dir)
+test_set = torch.load(test_dir)
+
+local classes = {'food','bird'}
+
+cnn = buildAlexNet()
+
+cutorch.setDevice(3)
+
+cnn = cnn:cuda()
+
+------------Loss Function -----
+
+crit = nn.ClassNLLCriterion()
+crit = crit:cuda()
+
+-------train NN-----
+
+trainIt = nn.StochasticGradient(cnn, crit)
+trainIt.learningRate = 0.001
+trainIt.maxIteration = 5
+train_set.data = train_set.data:cuda()
+
+trainIt.train(train_set)
+
+---------prediction ---------
+
+for i=1,#test_set do
+   pred = cnn:forward(test_set.data[i])
+   print(pred:exp())
+end
+
