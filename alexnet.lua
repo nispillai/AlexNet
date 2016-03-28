2dfb4690143c235e8703d893d925eba3037be7b0
Pillai <nmbpillai@gmail.com>
2016-03-28 00:57:21 -0400

    Update alexnet.lua

--- alexnet.lua
+++ alexnet.lua
@@ -1,16 +1,15 @@
+require 'nn'
+require 'torch'
 require 'cudnn'
+require 'image'
 
 
-function convLayer(a)
-   return cudnn.SpatialConvolution(a)
-end
-
-function maxPool(a)
-   return cudnn.SpatialMaxPooling(a)
+function maxPool(a,b,c,d)
+   return nn.SpatialMaxPooling(a,b,c,d)
 end
 
 function cuLinear(size1,size2)
-   return Linear(size1,size2)
+   return nn.Linear(size1,size2)
 end
 
 function Drop(a)
@@ -18,7 +17,7 @@ function Drop(a)
 end
 
 function lSMLayer()
-   return cudnn.LogSoftMax()
+   return nn.LogSoftMax()
 end
 
 function reLu() 
@@ -31,19 +30,19 @@ end
 
 function buildAlexNet(nClasses)
    torch.setdefaulttensortype('torch.FloatTensor')
-
    local cnn = nn.Sequential()
-   cnn:add(convLayer(3,96,11,11,4,4,2,2))
+   local cnn2 = nn.Sequential()
+   cnn:add(nn.SpatialConvolution(3,96,11,11,4,4,2,2))
    cnn:add(reLu())   
    cnn:add(maxPool(3,3,2,2))
-   cnn:add(convLayer(96,256,5,5,1,1,2,2))
+   cnn:add(nn.SpatialConvolution(96,256,5,5,1,1,2,2))
    cnn:add(reLu())
    cnn:add(maxPool(3,3,2,2))
-   cnn:add(convLayer(256,384,3,3,1,1,1,1))
+   cnn:add(nn.SpatialConvolution(256,384,3,3,1,1,1,1))
    cnn:add(reLu())
-   cnn:add(convLayer(384,384,3,3,1,1,1,1))
+   cnn:add(nn.SpatialConvolution(384,384,3,3,1,1,1,1))
    cnn:add(reLu())
-   cnn:add(convLayer(384,256,3,3,1,1,1,1))
+   cnn:add(nn.SpatialConvolution(384,256,3,3,1,1,1,1))
    cnn:add(reLu())   
    cnn:add(maxPool(3,3,2,2))
    cnn:cuda()
@@ -58,23 +57,103 @@ function buildAlexNet(nClasses)
    
    cnn1:add(thresh(0,1e-6))   
    cnn1:add(cuLinear(4096,nClasses)) 
-   cnn1:add(lSMLayer)
+   cnn1:add(lSMLayer())
    cnn1:cuda()
    
    local model = nn.Sequential():add(cnn):add(cnn1)
-   print(model)
    return model
 end
+
+function getDirectories(dir,noDir)
+   chk = 1
+   dirNames = {}
+   local p = io.popen('ls ' .. dir)
+   for file in p:lines() do
+      dirNames[chk] = file
+      if chk == noDir then
+         io.flush()
+         return dirNames
+      end
+      chk = chk + 1
+   end
+   io.flush()
+   return dirNames
+end
+
+function fileNumbers(dir,dirNames)
+   fTot = 0
+   for c = 1,#dirNames do
+      file = dirNames[c]
+      dir1  = dir .. "/" .. file
+      local pq = io.popen('find "'..dir1..'" -type f')
+      for fpq in pq:lines() do
+         fTot = fTot + 1
+      end
+      io.flush()
+   end
+   return fTot
+end
+function dirLookup(dir,noDir)
+
+   classes = {}
+   ij = 1
+   imTot = 1
+
+   local dirNames = getDirectories(dir,noDir)
+   local fTot = fileNumbers(dir,dirNames)
+   local imagesAll = torch.Tensor(fTot,3,400,200)
+   local labelsAll = torch.Tensor(fTot)
+
+   local labelNo = 1
+   for c = 1,#dirNames do
+      file = dirNames[c]
+      f1 = string.sub(file, 2)
+      classes[ij] = f1
+      ij = ij + 1
+
+      dir1  = dir .. "/" .. file
+      local pq = io.popen('find "'..dir1..'" -type f')
+      for fpq in pq:lines() do
+         ok = image.load(fpq)
+         it = image.scale(ok,400,200)
+         if it:size(1) == 3 then
+            imagesAll[labelNo] = it
+            labelsAll[labelNo] = f1
+            labelNo = labelNo + 1
+         end
+      end
+      io.flush()
+    end
+-- create train set:
+   trainset = {
+      data = torch.Tensor(labelNo, 3, 400, 200),
+      label = torch.Tensor(labelNo),
+      size = function() return labelNo end
+   }
+   setmetatable(trainset,
+    {__index = function(t, i)
+                    return {t.data[i], t.label[i]}
+                end}
+   );
+  for ijk = 1,labelNo do
+         trainset.data[ijk] = imagesAll[ijk]:clone()
+         trainset.label[ijk] = labelsAll[ijk]
+   end
+   trainset.data = trainset.data:double()
+   return classes,trainset
+end
+
+
 
 
 ------------------load images & classes ----
 local train_dir = '/hdd2/datasets/imagenet/train256max/'
 local test_dir = '/hdd2/datasets/imagenet/val/'
 
-train_set = torch.load(train_dir)
-test_set = torch.load(test_dir)
+local classes , trainset = dirLookup(train_dir,2)
+---test_set = torch.load(test_dir)
 
-local classes = {'food','bird'}
+print(classes)
 
 cnn = buildAlexNet(1000)
 
@@ -82,6 +161,9 @@ cutorch.setDevice(3)
 
 cnn = cnn:cuda()
 
+print 'model:'
+print(cnn)
+os.exit()
 ------------Loss Function -----
 
 crit = nn.ClassNLLCriterion()
